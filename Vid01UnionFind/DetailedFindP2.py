de94bf63ae555b6d3bd9a7f7e0157c25de074ae8
Robbie Hammond <robbiehammond3@gmail.com>
2023-05-31 20:20:57 -0400

    Find explanation done! Hell yeah

--- Vid01UnionFind/DetailedFindP2.py
+++ Vid01UnionFind/DetailedFindP2.py
@@ -26,5 +26,52 @@ class DetailedFind(Scene):
         self.wait(1)
 
 
+        self.play(g.animate.add_edges(*[(1, 0)]), g[1].animate.set_stroke(WHITE, width=0), run_time=0.75)
+        self.play(g.animate.add_edges(*[(2, 1)]), g[2].animate.set_stroke(WHITE, width=0), run_time=0.75)
 
+        self.wait(2)
+
+        self.play(g.animate.shift(4 * LEFT))
+
+        default_width = g[0].get_width()
+
+        find = Tex('find(2) = ?', font_size = 96).shift(2 * UP + 3 * RIGHT)
+        self.play(Create(find))
+
+        
+        find2 = Tex('parent[2] = 1', font_size = 72).shift(0.5 * UP + 3 * RIGHT)
+        self.play(g[2].animate.set_width(1.2))
+        self.play(Create(find2))
         self.wait(1)
+
+        find1 = Tex('parent[1] = 0', font_size = 72).shift(0.5 * DOWN, 3 * RIGHT)
+        self.play(g[2].animate.set_width(default_width), g[1].animate.set_width(1.2))
+        self.play(Create(find1))
+        self.wait(1)
+
+        find0 = Tex('parent[0] = 0', font_size = 72).shift(1.5 * DOWN, 3 * RIGHT)
+        self.play(g[1].animate.set_width(default_width), g[0].animate.set_width(1.2))
+        self.play(Create(find0))
+        self.wait(1)
+
+        findComplete = Tex('find(2) = 0', font_size = 96).shift(2 * UP + 3 * RIGHT)
+        self.play(g[0].animate.set_width(default_width))
+        self.play(ReplacementTransform(find, findComplete))
+
+        self.wait(3)
+
+        self.play(FadeOut(g), FadeOut(findComplete), FadeOut(find), FadeOut(find2), FadeOut(find1), FadeOut(find0))
+
+        code = '''
+Find(a) -> Element {
+    if (parent[a] != a)
+        return Find(parent[a]);
+    return a;
+}
+'''
+
+        rendered_code = Code(code=code, tab_width=4, background="window", font_size=36, language="C++",
+                                    font="Monospace")
+        self.play(FadeIn(rendered_code))
+
+        self.wait(5)
